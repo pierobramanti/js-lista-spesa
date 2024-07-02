a08791bc5932aa7691a967b21177244dd69921c4
pierobramanti <piero.bramanti94@gmail.com>
2024-07-02 16:36:51 +0200

    [UPD] created ciclo while

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// CREO ARRAY //
+let ingredients = ["Pomodorini", "Tonno in scatola", "Aglio", "Accighe sott'olio", "Capperi", "Olive taggiasche", "Prezzemolo", "Bottarga", "Spaghettoni" ,"Sale Grosso"]
+// CREO VARIABILE RECUPERANDO ID "UL"//
+let shopping_list = document. getElementById("shopping-list")
+
+// CREO CICLO WHILE //
+let i= 0
+
+while ( i <ingredients.lenght ){
+    let li= document.createElement("li")
+    li.innerHTML= ingredients[i]
+    shopping_list = append(li)
+    i++
+}
